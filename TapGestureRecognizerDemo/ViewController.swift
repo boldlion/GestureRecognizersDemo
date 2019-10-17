de13a19d5540571e54df79d72fae4946c5dc235a
Bold Lion <boldlion1988@gmail.com>
2019-10-17 16:21:26 +0300

    Initial Commit

---
+++ TapGestureRecognizerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapGestureRecognizerDemo
+//
+//  Created by Bold Lion on 17.10.19.
+//  Copyright © 2019 Bold Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
